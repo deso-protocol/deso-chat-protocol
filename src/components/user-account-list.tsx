755f39a49553a9f4ee106c05f77e426fb5120ca8
stas-kh <stas@deso.org>
2023-02-10 16:30:32 +0200

    chore: polishing ui for user list

--- src/components/user-account-list.tsx
+++ src/components/user-account-list.tsx
@@ -7,10 +7,12 @@ import { UserContext } from "../contexts/UserContext";
 import { identity } from "@deso-core/identity";
 import { desoAPI } from "../services/desoAPI.service";
 import { ClipLoader } from "react-spinners";
+import { MenuItem } from "@material-tailwind/react";
+import { toast } from "react-toastify";
 
 const COLLAPSED_ACCOUNTS_NUM = 3;
 const EXPANDED_ACCOUNTS_NUM = 10;
-const ACCOUNT_LIST_ITEM_HEIGHT_PX = 40;
+const ACCOUNT_LIST_ITEM_HEIGHT_PX = 30;
 
 type Account = {
   name: string;
@@ -19,45 +21,67 @@ type Account = {
   profile: User;
   hasProfile: boolean;
   isActive: boolean;
-}
+};
 
 const UserAccountList = () => {
   const { appUser } = useContext(UserContext);
   const [showMore, setShowMore] = useState<boolean>(false);
   const [users, setUsers] = useState<User[]>([]);
   const [allAccounts, setAllAccounts] = useState<Account[]>([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    // TODO: undecided about whether to show loader or show public keys before
-    // we've fetched data from backend. For now, show loader.
-    setLoading(true);
+    const fetchLoggedInUsers = async (loggedInUserKeys: Array<string>) => {
+      if (loggedInUserKeys.length === 0) {
+        return;
+      }
+
+      setLoading(true);
+
+      try {
+        const res = await desoAPI.user.getUsersStateless({
+          PublicKeysBase58Check: loggedInUserKeys,
+          SkipForLeaderboard: true,
+        });
+
+        if (res.UserList) {
+          setUsers(res.UserList);
+        }
+      } catch (e) {
+        toast.error(`Error fetching user profiles: ${e}`);
+        console.error(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     const snapshot = identity.snapshot();
     const activeUser = snapshot.currentUser;
     const alternateUsers = Object.keys(snapshot.alternateUsers || {});
 
-    const PublicKeysBase58Check = [...(activeUser ? [activeUser.publicKey] : []), ...alternateUsers];
-    setUsers(PublicKeysBase58Check.map((key) => ({ PublicKeyBase58Check: key })) as User[]);
-    desoAPI.user.getUsersStateless({
-      PublicKeysBase58Check,
-      SkipForLeaderboard: true,
-    }).then((res) => {
-      if (res.UserList) {
-        setUsers(res.UserList);
-      }
-    }).finally(() => setLoading(false));
+    const loggedInUserKeys = [
+      ...(activeUser ? [activeUser.publicKey] : []),
+      ...alternateUsers,
+    ];
+
+    setUsers(
+      loggedInUserKeys.map((key) => ({
+        PublicKeyBase58Check: key,
+      })) as User[]
+    );
+
+    fetchLoggedInUsers(loggedInUserKeys);
   }, [appUser]);
 
   useEffect(() => {
     const accounts = (users || []).map((user) => {
       const key = user.PublicKeyBase58Check;
       const isActive = key === appUser?.PublicKeyBase58Check;
-      const profile =
-        isActive && appUser ? appUser : user;
+      const profile = isActive && appUser ? appUser : user;
       const hasProfile = !!profile;
 
       return {
-        name: formatDisplayName(user),
+        name: formatDisplayName(user, ""),
         onclick: async (key: string) => identity.setActiveUser(key),
         key,
         profile,
@@ -78,66 +102,69 @@ const UserAccountList = () => {
     : allAccounts.slice(0, COLLAPSED_ACCOUNTS_NUM);
 
   return (
-    <div className="mb-0">
-      {
-        loading ?
-          <div className="flex justify-center">
-            <ClipLoader color={"#6d4800"} loading={true} size={16} />
-          </div> :
-          <>
-            <ul
-              className="text-gray-700 custom-scrollbar"
-              aria-labelledby="dropdownInformationButton"
-              style={
-                showMore
-                  ? {
-                      maxHeight: `${
-                        EXPANDED_ACCOUNTS_NUM * ACCOUNT_LIST_ITEM_HEIGHT_PX
-                      }px`,
-                      overflowY: "scroll",
-                    }
-                  : {}
-              }
-            >
-              {visibleAccounts.map((option) => {
-                return (
-                  <li
-                    key={option.key}
-                    onClick={() => option.onclick(option.key)}
-                    className="cursor-pointer text-md"
-                    style={{ height: `${ACCOUNT_LIST_ITEM_HEIGHT_PX}px` }}
+    <div
+      className={`mb-0 ${
+        loading || visibleAccounts.length > 0 ? "border-b" : ""
+      }`}
+    >
+      {loading ? (
+        <div className="flex justify-center my-2 h-[29px]">
+          <ClipLoader color={"#6d4800"} loading={true} size={24} />
+        </div>
+      ) : (
+        <>
+          <section
+            className="text-gray-700 custom-scrollbar"
+            aria-labelledby="dropdownInformationButton"
+            style={
+              showMore
+                ? {
+                    maxHeight: `${
+                      EXPANDED_ACCOUNTS_NUM * ACCOUNT_LIST_ITEM_HEIGHT_PX
+                    }px`,
+                    overflowY: "scroll",
+                  }
+                : {}
+            }
+          >
+            {visibleAccounts.map((option) => {
+              return (
+                <MenuItem
+                  key={option.key}
+                  onClick={() => option.onclick(option.key)}
+                  className="cursor-pointer text-md pl-2"
+                >
+                  <div
+                    className={`flex items-center ${
+                      option.isActive ? "font-bold" : ""
+                    }`}
                   >
-                    <div
-                      className={`flex items-center py-1 hover:bg-gray-100 ${
-                        option.isActive ? "font-bold" : ""
-                      }`}
-                    >
-                      <MessagingDisplayAvatar
-                        publicKey={option.key}
-                        username={option.name}
-                        classNames="mr-2 ml-0"
-                        diameter={28}
-                      />
-                      <div className="truncate">{option.name}</div>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
+                    <MessagingDisplayAvatar
+                      publicKey={option.key}
+                      username={option.name}
+                      classNames="mr-2 ml-0"
+                      diameter={28}
+                    />
+                    <div className="truncate text-base">{option.name}</div>
+                  </div>
+                </MenuItem>
+              );
+            })}
+          </section>
 
-            {!showMore && allAccounts.length > COLLAPSED_ACCOUNTS_NUM && (
-              <span
-                className="text-blue-600 text-sm text-left mx-4 my-2 block cursor-pointer"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setShowMore(true);
-                }}
-              >
-                Show more
-              </span>
-            )}
-          </>
-      }
+          {!showMore && allAccounts.length > COLLAPSED_ACCOUNTS_NUM && (
+            <span
+              className="text-blue-600 text-sm text-left mx-3 mt-2 mb-3 block cursor-pointer"
+              onClick={(e) => {
+                e.stopPropagation();
+                setShowMore(true);
+              }}
+            >
+              Show more
+            </span>
+          )}
+        </>
+      )}
     </div>
   );
 };
