8baab0728da3f88272a8deecb8dd9bbc4b9055c6
lazynina <lazynina84@gmail.com>
2023-02-10 01:56:19 -0500

    revert changes to desoAPI service

--- src/services/desoAPI.service.tsx
+++ src/services/desoAPI.service.tsx
@@ -2,7 +2,7 @@ import { Deso } from "deso-protocol";
 import { DESO_NETWORK } from "utils/constants";
 
 export const desoAPI = new Deso({
-  nodeUri: process.env.REACT_APP_NODE_URL + "/api/v0",
+  nodeUri: process.env.REACT_APP_API_URL,
   identityConfig: {
     uri: process.env.REACT_APP_IDENTITY_URL,
     network: DESO_NETWORK,
