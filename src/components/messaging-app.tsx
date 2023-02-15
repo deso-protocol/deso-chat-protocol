f24c28b200d376d875392eff17d6b893e1d6bc56
lazynina <lazynina84@gmail.com>
2023-02-15 12:57:03 -0500

    don't refresh if not online

--- src/components/messaging-app.tsx
+++ src/components/messaging-app.tsx
@@ -114,7 +114,8 @@ export const MessagingApp: FC = () => {
       if (
         !appUser ||
         !selectedConversationPublicKey ||
-        lockRefreshRef.current
+        lockRefreshRef.current ||
+        !navigator.onLine
       ) {
         return;
       }
